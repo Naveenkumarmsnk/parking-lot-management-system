82f258638bb2f9af911d3414bbb3a6279c316403
Naveenkumarmsnk <118828975+Naveenkumarmsnk@users.noreply.github.com>
2026-03-29 18:11:10 +0530

    Create TicketRepository.java

---
+++ src/main/java/com/parking/repository/TicketRepository.java
@@ -0,0 +1,11 @@
+package com.parking.repository;
+
+import com.parking.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    Optional<Ticket> findByVehicleNumberAndIsPaidFalse(String vehicleNumber);
+}
